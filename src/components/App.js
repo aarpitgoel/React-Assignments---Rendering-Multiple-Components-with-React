f8633a7a8396102e0975cc90caf9f7e98eaf8745
aarpitgoel <64373216+aarpitgoel@users.noreply.github.com>
2020-11-06 23:56:48 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <div data-ns-test="project-name">Vehicle service at home</div>
+      <div data-ns-test="project-description">Vehicle service at home</div>
+    </>
+  );
+};
 
 export default App;
